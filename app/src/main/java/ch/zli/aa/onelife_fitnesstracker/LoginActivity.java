b8c1fae19aaee85c1d458c872b56c17d411ab268
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2023-03-07 14:14:24 +0100

    Add login and signup functionality

--- app/src/main/java/ch/zli/aa/onelife_fitnesstracker/LoginActivity.java
+++ app/src/main/java/ch/zli/aa/onelife_fitnesstracker/LoginActivity.java
@@ -1,11 +1,18 @@
 package ch.zli.aa.onelife_fitnesstracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,9 +29,58 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword1);
+        btnLogin = findViewById(R.id.btnsubmit);
+
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "clicked on Login");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+        btnSignup.setOnClickListener(view -> {
+            goSignupActivity();
+            Animatoo.INSTANCE.animateSwipeLeft(this);
+        });
 
     }
 
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "to login user" + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Faild To login", e);
+                    return;
+                }
+                goMainActivity();
+            }
+        });
+    }
 
+    private void goMainActivity() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void goSignupActivity() {
+        Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(i);
+        finish();
+    }
 }
+
 
