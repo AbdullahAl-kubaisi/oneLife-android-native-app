31f675be4326b5f0229264b23ba77ad1fc2446cc
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2023-03-07 15:45:45 +0100

    connected workouts to database

--- app/src/main/java/ch/zli/aa/onelife_fitnesstracker/ParseApplication.java
+++ app/src/main/java/ch/zli/aa/onelife_fitnesstracker/ParseApplication.java
@@ -8,11 +8,13 @@ package ch.zli.aa.onelife_fitnesstracker;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Workout.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("GVedOgp9nNYDRLBQFK0j6UWxJNMi4JymZ3CWd8pE")
                 .clientKey("QCj12SkVPiOG43roxDYtN8e7U47xEOpQyXyFMefU")
