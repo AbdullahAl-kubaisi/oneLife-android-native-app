981ff7ae3efd0c06cd10d9acf6664ac74e49c27b
Al-Kubaisi Abdullah, GHR-OSA-NEX-18 <Abdullah.Al-Kubaisi@swisscom.com>
2023-03-07 21:05:50 +0100

    added method to switch the views

--- app/src/main/java/ch/zli/aa/onelife_fitnesstracker/MainActivity.java
+++ app/src/main/java/ch/zli/aa/onelife_fitnesstracker/MainActivity.java
@@ -1,13 +1,26 @@
 package ch.zli.aa.onelife_fitnesstracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseObject;
 
+import ch.zli.aa.onelife_fitnesstracker.fragments.HomeFragment;
+import ch.zli.aa.onelife_fitnesstracker.fragments.LearnFragment;
+import ch.zli.aa.onelife_fitnesstracker.fragments.ProfileFragment;
+import ch.zli.aa.onelife_fitnesstracker.fragments.TrackFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager mFragmentManager = getSupportFragmentManager();
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
     //            Log.e("MainActivity", e.getLocalizedMessage());
     //        }else{
     //            Log.d("MainActivity","Object saved.");
+
+        mBottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_learn:
+                        fragment = new LearnFragment();
+                        break;
+                    case R.id.action_track:
+                        fragment = new TrackFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                mFragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
-        }
+        });
+        mBottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+}
